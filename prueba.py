ef190c9544fbc585dc70cf68bedfbc11da03f79e
TAKZUIN <Takzuin@gmail.com>
2024-09-01 01:08:15 -0500

    Delete prueba.py

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import font
-
-ventana = tk.Tk()
-fuentes_disponibles = list(font.families())
-ventana.destroy()
-
-for fuente in fuentes_disponibles:
-    print(fuente)
